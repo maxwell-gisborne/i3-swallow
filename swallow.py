cbe948190c685145f869ec387d9aecd5c1469d7b
Maxwell Gisborne <maxwell+git@gisborne.xyz>
2023-08-04 16:27:41 +0100

    refactor

--- swallow.py
+++ swallow.py
@@ -1,53 +1,49 @@
 #!/usr/bin/python3
 
-import argparse
 import i3ipc
-from subprocess import Popen, PIPE
-import signal
+
+import argparse
 from sys import exit
 
-
-def on_window_event(args, process, swallowed_atom):
-    def event_listener(_, event):
-        process.poll()
-
-        swallowed = swallowed_atom[0]
-
-        if event.change == 'new' and not swallowed:
-            focused.command('focus; move scratchpad')
-            swallowed_atom[0] = True
-            if args.d:
-                i3.main_quit()
-
-        elif event.change == 'close' and swallowed:
-            focused.command('scratchpad show; floating toggle;')
-            exit()
-
-    return event_listener
+from subprocess import Popen, PIPE
+import signal
 
 
 parser = argparse.ArgumentParser(description="i3-swallow.")
 parser.add_argument("-d", action="store_true",
                     help="Don't return window on exit.")
 parser.add_argument("cmd", nargs="+", help="Command to be executed")
+args = parser.parse_args()
 
 
-if __name__ == "__main__":
-    args = parser.parse_args()
-    i3 = i3ipc.Connection()
-    focused = i3.get_tree().find_focused()
+i3 = i3ipc.Connection()
+focused = i3.get_tree().find_focused()
+swallowed_atom = [False]
 
-    process = Popen(args.cmd, stdout=PIPE)
-    process.send_signal(signal.SIGSTOP)
+process = Popen(args.cmd, stdout=PIPE)
+process.send_signal(signal.SIGSTOP)
+# seems to stop swallowing signal.signal(signal.SIGCHLD, lambda *_: '' if process.poll() else exit())
 
-    # The following seems to prevent swallowing?
-    #  signal.signal(signal.SIGCHLD, lambda *_: '' if process.poll() else exit())
 
-    listener = on_window_event(args, process, [False])
+def listener(_, event):
+    process.poll()
 
-    i3.on("window::new", listener)
-    if not args.d:
-        i3.on("window::close", listener)
+    swallowed = swallowed_atom[0]
 
-    process.send_signal(signal.SIGCONT)
-    i3.main()
+    if event.change == 'new' and not swallowed:
+        focused.command('focus; move scratchpad')
+        swallowed_atom[0] = True
+        if args.d:
+            i3.main_quit()
+
+    elif event.change == 'close' and swallowed:
+        focused.command('scratchpad show; floating toggle;')
+        exit()
+
+
+i3.on("window::new", listener)
+if not args.d:
+    i3.on("window::close", listener)
+
+process.send_signal(signal.SIGCONT)
+i3.main()
